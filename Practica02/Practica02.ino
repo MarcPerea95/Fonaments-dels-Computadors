94f9a048d78b3a02d9e06663ff65853c617db4ab
Marc Perea i Agustí <MarcPerea95@users.noreply.github.com>
2018-02-19 15:55:57 +0100

    Actualizacion Practica02

--- Practica02/Practica02.ino
+++ Practica02/Practica02.ino
@@ -1,6 +1,32 @@
 #define PIN_LED 2
 #define BUTTON 12
+bool activate = false;
+int prevState = LOW;
+int currState = LOW;
 
+int execTime = 0;
+
+class Timer
+{
+  public:
+    int currentTime;
+    Timer(){currentTime = 0;}
+    bool Ready(int t)
+    {
+      if(millis()-currentTime >= t)
+      {
+        currentTime = millis();
+        return true;
+      }
+
+      return false;
+      
+      
+    }
+};
+Timer t1;
+Timer t2;
+Timer t3;
 
 // the setup function runs once when you press reset or power the board
 void setup() {
@@ -12,16 +38,26 @@ void setup() {
 
 // the loop function runs over and over again forever
 void loop() {
-  boolean activate = false;
-  if (digitalRead(BUTTON) == HIGH){
+  currState = digitalRead(BUTTON);
+
+  if (currState == HIGH && currState != prevState) {
     activate = !activate;
   }
 
+  prevState = currState;
+
+
   if (activate) {
-    digitalWrite(PIN_LED, HIGH);   // turn the LED on (HIGH is the voltage level)
-    delay(1000);             // wait for a second
-    digitalWrite(PIN_LED, LOW);    // turn the LED off by making the voltage LOW
-    delay(1000);       // wait for a second
+    //int timer
+    if(t1.Ready(100))
+    digitalWrite(PIN_LED, HIGH);     // turn the LED on (HIGH is the voltage level)
+    //millis(1000);
+    if(t2.Ready(200))// wait for a second
+    digitalWrite(PIN_LED, LOW);      // turn the LED off by making the voltage LOW
+    //millis(1000);                    // wait for a second
 
   } else {}
 }
+
+
+
