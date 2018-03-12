e2c6cb6e48c28ea0068afff224213d4e6e6b88fd
Axel Castells <9480855+axcamo@users.noreply.github.com>
2018-03-12 15:15:06 +0100

    Semaphors Finished

--- PracticaSemafor/main/main.ino
+++ PracticaSemafor/main/main.ino
@@ -5,13 +5,13 @@
 #define PIN_RED_PED 6
 #define PIN_BUTTON 7
 
-enum SemaphorStates {GREEN, ORANGE, RED};
+enum SemaphorStates {SETUP, S1, S1F, S2, S3, S4, S5};
 enum Modes {NORMAL, FORCED};
 
 class Timer
 {
   private:
-    int currentTime;
+    long currentTime;
   public:
     Timer() {
       currentTime = millis();
@@ -27,14 +27,19 @@ class Timer
         return false;
       }
     }
+    float Remaining(int t)
+    {
+      return t - (millis() - currentTime);
+    }
 };
 
 Timer timer;
-SemaphorStates semStateCar, semStatePed;
+SemaphorStates semState;
 Modes mode;
 
 // the setup function runs once when you press reset or power the board
 void setup() {
+  Serial.begin(9600);
   // initialize digital pin 2 as an output.
   pinMode(PIN_GREEN_CAR, OUTPUT);
   pinMode(PIN_ORANGE_CAR, OUTPUT);
@@ -42,16 +47,89 @@ void setup() {
   pinMode(PIN_GREEN_PED, OUTPUT);
   pinMode(PIN_RED_PED, OUTPUT);
   pinMode(PIN_BUTTON, INPUT);
+
+
+  digitalWrite(PIN_GREEN_CAR, HIGH);
+  digitalWrite(PIN_RED_PED, HIGH);
   timer = Timer();
+  semState = S1;
 
   //digitalWrite(PIN_GREEN_PED, HIGH);
 }
 
 // the loop function runs over and over again forever
 void loop() {
-  if(digitalRead(PIN_BUTTON) == HIGH)digitalWrite(PIN_GREEN_CAR, HIGH);
-  //do{digitalWrite(PIN_GREEN_CAR, HIGH);}while(false);
-   //if(digitalRead(PIN_BUTTON) == LOW)digitalWrite(PIN_GREEN_CAR, HIGH);
+  Serial.println(semState);
+
+  
+  Serial.flush();
+
+  switch (semState) {
+    case S1:
+      {
+        Serial.println(timer.Remaining(20000));
+        bool pushedButton = digitalRead(PIN_BUTTON);
+        if (pushedButton && timer.Remaining(20000) >= 5000)
+        {
+          semState = S1F;
+          timer = Timer();
+        }
+        else if (timer.Ready(20000)) {
+          semState = S2;
+          digitalWrite(PIN_GREEN_CAR, LOW);
+          digitalWrite(PIN_ORANGE_CAR, HIGH);
+          timer = Timer();
+        }
+      } break;
+    case S1F:
+      {
+        if (timer.Ready(5000))
+        {
+          semState = S2;
+          digitalWrite(PIN_GREEN_CAR, LOW);
+          digitalWrite(PIN_ORANGE_CAR, HIGH);
+          timer = Timer();
+        }
+      } break;
+    case S2:
+      {
+        if (timer.Ready(3000)) {
+          semState = S3;
+          digitalWrite(PIN_RED_CAR, HIGH);
+          digitalWrite(PIN_ORANGE_CAR, LOW);
+          timer = Timer();
+        }
+      } break;
+    case S3:
+      {
+        if (timer.Ready(1000)) {
+          semState = S4;
+          timer = Timer();
+          digitalWrite(PIN_GREEN_PED, HIGH);
+          digitalWrite(PIN_RED_PED, LOW);
+
+        }
+      } break;
+    case S4:
+      {
+        if (timer.Ready(10000)) {
+          semState = S5;
+          digitalWrite(PIN_GREEN_PED, LOW);
+          digitalWrite(PIN_RED_PED, HIGH);
+          timer = Timer();
+        }
+      } break;
+    case S5:
+      {
+        if (timer.Ready(1000)) {
+          semState = S1;
+          timer = Timer();
+          digitalWrite(PIN_RED_CAR, LOW);
+          digitalWrite(PIN_GREEN_CAR, HIGH);
+        }
+      } break;
+
+  }
 }
 
 
