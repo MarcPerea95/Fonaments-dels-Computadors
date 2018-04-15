308bc6aa33cc5a38d21d5c29e99b798f566ea6ed
Marc Perea i Agustí <marcpa95@gmail.com>
2018-04-15 18:07:04 +0200

    comunicació ARDUINO-processing

--- PracticaAvio/simulador_avio_arduino/simulador_avio_arduino.ino
+++ PracticaAvio/simulador_avio_arduino/simulador_avio_arduino.ino
@@ -1,17 +1,19 @@
+#include "comm.h"
+
 #define BUZZER_PIN  3
-#define TRIGGER_PIN 9 
-#define ECHO_PIN 5  
+#define TRIGGER_PIN 9
+#define ECHO_PIN 5
 #define SENSOR_PIN 2
 #define MOTOR_PIN 10
 
-long t; //temps que triga el eco en rebotar
-long d; //distancia recorreguda en cm
+long temps; //temps que triga el eco en rebotar
+long distancia; //distancia recorreguda en cm
 
 int valorVelocitat;
 
 
 void setup() {
-  
+
   Serial.begin(9600);
   //ALTIMETRE
   pinMode(TRIGGER_PIN, OUTPUT);
@@ -23,14 +25,25 @@ void setup() {
   digitalWrite(SENSOR_PIN, HIGH);
 
   //ALARMA
-  pinMode(BUZZER_PIN,OUTPUT);
+  pinMode(BUZZER_PIN, OUTPUT);
 
   //MOTOR
   pinMode(MOTOR_PIN, OUTPUT);
   //valorVelocitat = 0;
+
+  commSetup();
 }
 
 void loop() {
+  
+  //COMUNICACIÓ
+  if ( portIsConnected() ) {
+    sendData (CHANNEL_1, distancia);
+    sendData (CHANNEL_2, digitalRead(SENSOR_PIN));
+    if (dataAvailable(CHANNEL_3) ) {
+      valorVelocitat = getData(CHANNEL_3);
+    }
+  }
 
   //ALTIMETRE
   digitalWrite(TRIGGER_PIN, LOW);
@@ -41,17 +54,18 @@ void loop() {
   delayMicroseconds(10); //Enviem un pols de 10 microsegons
   digitalWrite(TRIGGER_PIN, LOW);
 
-  t = pulseIn(ECHO_PIN, HIGH); //obtenim el ancho de pulso
-  d = (t / 59) * 10000; //escalem el temps a una distancia en cm
+  temps = pulseIn(ECHO_PIN, HIGH); //obtenim el ancho de pulso
+  distancia = (temps / 59) * 10000; //escalem el temps a una distancia en cm
 
   Serial.print("Distancia: ");
-  Serial.print(d);  //imprimim la distancia
+  Serial.print(distancia);  //imprimim la distancia
   Serial.print("metres");
   Serial.println();
   delay(100); //fem una pausa de 100ms
 
 
   //SENSOR INCLINACIO + BUZZER
+
   if (digitalRead(SENSOR_PIN))   {
     //MASSA INCLINACIÓ
     Serial.print("INCLINACIÓ MÀXIMA: ALERTA");
@@ -69,9 +83,10 @@ void loop() {
   //MOTOR
   for (int i = 0; i < 255; i++)
   {
-     analogWrite(MOTOR_PIN, i);
+    analogWrite(MOTOR_PIN, i);
   }
- 
+  
+  commManager();
 }
 
 
