6e53262511cae22f0ea57954b349e29a90b60342
Axel Castells <9480855+axcamo@users.noreply.github.com>
2018-06-12 18:58:29 +0200

    Software Done

--- LaserTower/main/main.ino
+++ LaserTower/main/main.ino
@@ -1,8 +1,14 @@
+#include <Servo.h>
+
+//Pins
 #define LASER_PIN 7
 #define ALTIMETER_ECHO_PIN 5
 #define ALTIMETER_TRIGGER_PIN 6
 #define BUTTON_PIN 13
+#define SERVO_H 9
+#define SERVO_V 10
 
+//Constants
 #define TIME_INTERVAL 10
 #define CLEAR_INTERVAL 4
 #define SOUND_SPEED 292
@@ -25,6 +31,8 @@ LED_MODE_GATE laserMode;
 long lastDistance;
 boolean lastButtonRead;
 
+Servo servoH, servoV;
+
 void setup() {
   Serial.begin(9600);
 
@@ -33,18 +41,22 @@ void setup() {
 
   pinMode(LASER_PIN, OUTPUT);
   pinMode(ALTIMETER_TRIGGER_PIN, OUTPUT);
+  pinMode(SERVO_H, OUTPUT);
+  pinMode(SERVO_V, OUTPUT);
 
   pinMode(ALTIMETER_ECHO_PIN, INPUT);
   pinMode(BUTTON_PIN, INPUT);
 
-
   SetLaserState(HIGH);
   lastButtonRead = LOW;
   laserMode = ON;
+
+  servoH.attach(SERVO_H);
+  servoV.attach(SERVO_V);
 }
 
 void loop() {
-  
+
   boolean tempButtonRead = digitalRead(BUTTON_PIN);
   if (tempButtonRead == HIGH && lastButtonRead != HIGH)
   {
@@ -68,13 +80,27 @@ void loop() {
         {
           Serial.print("Gato a la vista! Distancia: ");
           Serial.println(abs(lastDistance - distance));
-          //towerMode = MOVING;
+
+          Serial.println("MOVING . . .");
+          towerMode = MOVING;
         }
 
         lastDistance = distance;
       } break;
     case MOVING:
       {
+        int rotH = random(0, 180);
+        int rotV = random(20, 160);
+
+        servoH.write(rotH);
+        servoV.write(rotV);
+
+        if (servoH.read() == rotH && servoV.read() == rotV)
+        {
+          Serial.println("RESET!");
+          towerMode = INITIALIZING;
+        }
+
       } break;
   }
 
